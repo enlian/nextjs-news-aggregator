8a744de7f44eafc5dba977a51dfcf92a978cf087
enlian <frejaenlian@gmail.com>
2024-11-22 23:54:50 +0900

    add detail list

---
+++ app/details/DetailItem.tsx
@@ -0,0 +1,14 @@
+const DetailItem = () => {
+  return (
+    <div>
+      <div>
+        <div>11月15日 昨天</div>
+        <div>
+          <i>出</i>100 <i>入</i>50
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailItem;
--- app/details/page.tsx
+++ app/details/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+import { Virtuoso } from 'react-virtuoso';
+import DetailItem from "./DetailItem";
+
 export default function name() {
-  return <>明细页面</>;
+  const items = [1, 2, 3, 4, 5, 6, 4, 8];
+  const list = Array.from({ length: 1000 }, (_, index) => `Item ${index + 1}`);
+
+  return (
+    <Virtuoso
+      totalCount={items.length}
+      itemContent={(index) => <div style={{ padding: 10 }}>Row {items[index]}</div>}
+      style={{ height: '400px', width: '300px' }}
+    />
+  );
 }
