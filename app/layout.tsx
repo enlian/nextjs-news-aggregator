f377a96830a0ac09759a6ee5870fb1f0298f1f72
enlian <frejaenlian@gmail.com>
2024-12-08 23:42:38 +0900

    add root Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import "./globals.css";
 import { BiSolidDetail } from "react-icons/bi";
 import { FaChartPie } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 export default function RootLayout({
   children,
@@ -21,39 +23,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
-        <div className="bottom-tabs">
-          <Link
-            href="/details"
-            className={`tab-item ${
-              isActive("details") ? "tab-item-active" : ""
-            }`}
-          >
-            <BiSolidDetail size={25} />
-            <span>明细</span>
-          </Link>
-          <Link
-            href="/statistics"
-            className={`tab-item ${
-              isActive("statistics") ? "tab-item-active" : ""
-            }`}
-          >
-            <FaChartPie size={23} />
+        <Provider store={store}>
+          <Header />
+          {children}
+          <div className="bottom-tabs">
+            <Link
+              href="/details"
+              className={`tab-item ${
+                isActive("details") ? "tab-item-active" : ""
+              }`}
+            >
+              <BiSolidDetail size={25} />
+              <span>明细</span>
+            </Link>
+            <Link
+              href="/statistics"
+              className={`tab-item ${
+                isActive("statistics") ? "tab-item-active" : ""
+              }`}
+            >
+              <FaChartPie size={23} />
 
-            <span>统计</span>
-          </Link>
-          <Link
-            href="/settings"
-            className={`tab-item ${
-              isActive("settings") ? "tab-item-active" : ""
-            }`}
-          >
-            <IoSettingsSharp size={25} />
+              <span>统计</span>
+            </Link>
+            <Link
+              href="/settings"
+              className={`tab-item ${
+                isActive("settings") ? "tab-item-active" : ""
+              }`}
+            >
+              <IoSettingsSharp size={25} />
 
-            <span>设置</span>
-          </Link>
-        </div>
+              <span>设置</span>
+            </Link>
+          </div>
+        </Provider>
       </body>
     </html>
   );
